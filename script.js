887adfce8498eeda65cd744df6cc143abff6cffe
prabin <prabin20panta@gmail.com>
2023-04-02 15:57:24 +0545

    added capital city data calculation

--- script.js
+++ script.js
@@ -8,29 +8,37 @@ const statusImg = document.querySelector('.status-img');
 const status = document.querySelector('.status');
 const timeStatus = document.querySelector('.as-of');
 const weatherContainer = document.querySelector('.weather-container');
+const countryInput = document.querySelector('#country-name');
+const countryForm = document.querySelector('.country-form');
 
 // Getting cuttent position
 const getCurrentPosition = () => {
-    if (navigator.geolocation){
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             function (position) {
-               const {latitude : lat, longitude : lng } = position.coords;
-               getWeatherDetails(lat,lng);
+                const { latitude: lat, longitude: lng } = position.coords;
+                getWeatherDetails(lat, lng);
             },
             function () {
                 alert('Could not get your position');
             }
         );
     }
-        
+
 }
 // Initialize get function
 getCurrentPosition();
 
 // Render the data
-const loadWeatherData = data =>{
+const loadWeatherData = (data,capital) => {
     weatherContainer.classList.remove('hide');
-    loc.innerText = data.location.name;
+    console.log(capital);
+    if(!capital){
+        loc.innerText = data.location.name;
+    }else{
+        const res = capital.charAt(0).toUpperCase() + capital.slice(1).toLowerCase();
+        loc.innerText = res;
+    }
     temp.innerText = data.current.temp_c;
     humid.innerText = data.current.humidity;
     wind.innerText = data.current.wind_kph;
@@ -41,12 +49,37 @@ const loadWeatherData = data =>{
 };
 
 // API call 
-const getWeatherDetails = async (lat,lng) =>{
-    try{
+const getWeatherDetails = async (lat, lng, capital) => {
+    try {
         const req = await fetch(`https://api.weatherapi.com/v1/current.json?key=a3338513d0294710acf180535230104 &q=${lat},${lng}&aqi=yes`);
         const data = await req.json();
-        loadWeatherData(data);
-    }catch(err){
+        if(capital){
+            loadWeatherData(data,capital);
+        }else{
+            loadWeatherData(data,'');
+        }
+    } catch (err) {
         console.error(`Error: ${err}`);
     }
 }
+
+
+const getCityData = async (capital) => {
+    try {
+        const res = await fetch(`https://restcountries.com/v3.1/capital/${capital}`);
+        const cityData = await res.json();
+        console.log(cityData);
+        getWeatherDetails(...(cityData[0].latlng),capital);
+        countryInput.value = '';
+    } catch (err) {
+        alert(`Error: COuld not get the city please try again`);
+    }
+}
+
+// Country Form
+countryForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = countryInput.value;
+    getCityData(data);
+});
